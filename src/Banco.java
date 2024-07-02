f572c190619f89d6fbf561eea63edf3ac8d48975
Gabi <dugabrielle9@gmail.com>
2024-07-02 16:30:01 -0300

    add dados

---
+++ src/Banco.java
@@ -0,0 +1,17 @@
+public class Banco {
+
+        public static void main(String[] args) {
+            String cliente = "Cliente 1";
+            String conta = "Corrente";
+            double saldo = 2448.50;
+
+            // Dados do cliente 1
+
+            System.out.println("**********************");
+            System.out.println("Cliente: " + cliente);
+            System.out.println("Conta: " + conta);
+            System.out.println("Saldo atual: " + saldo);
+            System.out.println("**********************");
+
+        }
+    }
